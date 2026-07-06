6dae0616c8f5bde7349d2e59af932f99ec8c7218
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-06 15:01:12 +0530

    feat: add popcorn pricing system based on size configurations

---
+++ PracticeQuestion4/app.js
@@ -0,0 +1,16 @@
+let popcornSize="XL"
+if(popcornSize==="XL"){
+    console.log("popcorn price is Rs.250")
+}
+ else if(popcornSize==="L"){
+    console.log("popcorn price is Rs.200")
+}
+else if(popcornSize==="M"){
+    console.log("popcorn price is Rs.100")
+}
+ else if(popcornSize==="S"){
+    console.log("popcorn price is Rs.50")
+}
+else{
+    console.log("Not available")
+}
