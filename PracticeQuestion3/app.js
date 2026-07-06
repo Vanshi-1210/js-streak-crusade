8d9b4ab5a50bc6349dccb36a7e9eb3eea9c96ccc
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-06 14:47:23 +0530

    feat: add traffic light system with fallback handling using else

---
+++ PracticeQuestion3/app.js
@@ -0,0 +1,13 @@
+let lightColor="violet"
+if(lightColor=="red"){
+    console.log("Stop Light is red")
+}
+else if(lightColor=="yellow"){
+    console.log("Slow down Light color is yellow")
+}
+else if(lightColor=="green"){
+    console.log("Go Light color is green")
+}
+else{
+    console.log("Traffic light is broken")
+}
