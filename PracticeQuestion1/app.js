b2a030ba819bb765ee433afa2e27bb02fe3bc713
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-06 14:16:44 +0530

    feat: add traffic light system using conditional if statements

---
+++ PracticeQuestion1/app.js
@@ -0,0 +1,11 @@
+//traffic light system.
+let lightColor="red";
+if(lightColor==="red"){
+    console.log("Stop! Light color is red.")
+}
+if(lightColor==="yellow"){
+    console.log("Slow down Light color is yellow")
+}
+if(lightColor==="green"){
+    console.log("Go Light color is green")
+}
