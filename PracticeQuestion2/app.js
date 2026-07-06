6e6fc40850c7fe013df8934878644ace29691b23
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-06 14:33:58 +0530

    feat: add student grading system using if-else if ladder and logical operators

---
+++ PracticeQuestion2/app.js
@@ -0,0 +1,13 @@
+let marks=99;
+if(marks>=80){
+    console.log("A+")
+}
+else if(marks>=60 && marks<80){
+    console.log("A")
+}
+else if(marks>=33 && marks<60){
+    console.log("B")
+}
+else{
+    console.log("F")
+}
