cf02f535d5638f5da05a813c518ccf4fe315ba76
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-06 15:08:56 +0530

    feat: implement student performance evaluation using nested if-else statements

---
+++ PracticeQuestion5/app.js
@@ -0,0 +1,12 @@
+let marks=89;
+if(marks>=33){
+    if(marks>80){
+        console.log("Outstanding")
+    }
+    else{
+        console.log("A")
+    }
+}
+else{
+    console.log("Better luck next time")
+}
