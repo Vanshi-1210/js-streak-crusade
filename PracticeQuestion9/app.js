5320c1d04a53908f60258bd8e85e313580177760
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-07 13:49:33 +0530

    feat: implement user alerts, prompts, and console logging methods

---
+++ PracticeQuestion9/app.js
@@ -0,0 +1,10 @@
+alert("something gone wrong");
+console.error("this is an error msg");
+console.warn("this is a warning");
+
+let firstName=prompt("Enter your first name");
+console.log(firstName);
+let lastName=prompt("Enter your last name");
+console.log(lastName);
+fullName=`Welcome ${firstName+" "+lastName}!`
+alert(fullName);
