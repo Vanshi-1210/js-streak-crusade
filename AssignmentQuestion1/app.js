bbc0ec8acd6505e96907b6fd8f6fe442e32c4ec0
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-07 14:28:21 +0530

    feat: implement user name and age prompt with template literal alert

--- AssignmentQuestion1/app.js
+++ AssignmentQuestion1/app.js
@@ -1,14 +1,19 @@
 // Question1
 // Qs1. Create a number variable num with some value.
 // Now, print "good" if the number is divisible by 10 and print "bad" if it is not.
-let num=20;
-if(num%10===0){
-    console.log("good");
-}
-else{
-    console.log("bad");
-}
+// let num=20;
+// if(num%10===0){
+//     console.log("good");
+// }
+// else{
+//     console.log("bad");
+// }
 
-
+// Qs2. Take the user's name & age as input using prompts.
+// Then return back the following statement to the user as an alert (by substituting their name & age):
+let name=prompt("Enter your name here");
+let age=prompt("Enter your age here");
+let output=`${name} is ${age} years old`;
+alert(output);
 
 
