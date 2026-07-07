4775113b86c23d805550caf22f690a3f604941c3
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-07 11:23:27 +0530

    feat: implement traffic light system using switch case statements

---
+++ PracticeQuestion7/app.js
@@ -0,0 +1,10 @@
+let color="purple";
+switch(color){
+    case "red":console.log("Stop");
+    break;
+    case "yellow":console.log("Slow down");
+    break;
+    case "green":console.log("Go");
+    break;
+    default:console.log("light is broken");
+}
