f5eb5715f3a68a48ec64d516ca5fb096aae6a53f
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-08 16:50:31 +0530

    feat: add solution for finding element index in a reversed array

--- PracticeQuestion12/app.js
+++ PracticeQuestion12/app.js
@@ -6,3 +6,13 @@ let months=['january','july','march','august'];
 months.splice(0,1);
 months.splice(1,0,'june');
 console.log(months);
+
+
+// Qs. Return the index of the "javascript" from the given array, if it was reversed.
+// Array: ['c', 'c++', 'html', 'javascript', 'python', 'java', 'c#', 'sql']
+
+
+let languages=['c','c++','html','javascript','python','java','c#','sql'];
+languages.reverse();
+console.log(languages);
+console.log(languages.indexOf('javascript'));
