93558fa6d698d6c02ae2a343756d9398c1859e7c
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-09 14:39:21 +0530

    feat: add solution to print even numbers between 2 to 10

--- PracticeQuestion15/app.js
+++ PracticeQuestion15/app.js
@@ -11,3 +11,11 @@ for(let i=1;i<=15;i++){
         console.log(i);
     }
 }
+
+// Qs.3 print all even numers from 2 to 10.
+
+for(let i=2;i<=10;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
