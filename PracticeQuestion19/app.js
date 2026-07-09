60af7f1c21fd0646e0c82d6e42a7603816f7467d
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-09 19:02:44 +0530

    feat: implement string iteration using for...of loop

--- PracticeQuestion19/app.js
+++ PracticeQuestion19/app.js
@@ -1,4 +1,11 @@
+//array
 let fruits=["apple","mango","banana","litchi","orange"];
 for(fruit of fruits){
     console.log(fruit);
+}
+
+//string
+let str="apnacollege";
+for(char of str){
+    console.log(char);
 }
