f0485bdb3f4f183d93dfef6b37421d7d6a482b37
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-09 18:50:42 +0530

    feat: implement nested loop to iterate over nested arrays

--- PracticeQuestion18/app.js
+++ PracticeQuestion18/app.js
@@ -3,3 +3,13 @@ fruits.push("pineapple");
 for(let i=0;i<fruits.length;i++){
     console.log(i,fruits[i]);
 }
+
+//nested loop with nested arrays.
+let heroes=[["ironman","spiderman","thor"],["superman","wonderman","flash"]];
+for(let i=0;i<heroes.length;i++){
+
+    console.log(`list #${i}`);
+    for(let j=0;j<heroes[i].length;j++){
+        console.log(heroes[i][j]);
+    }
+}
