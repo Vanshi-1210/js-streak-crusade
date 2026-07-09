5b7d7bda0a9106bc00fe58059aa6fb8b017e67f7
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-09 13:10:10 +0530

    feat: add solution to check if an element exists in an array

---
+++ PracticeQuestion14/app.js
@@ -0,0 +1,5 @@
+// Qs6. Write a JavaScript program to check if an element exists in an array or not.
+
+let array=["Vanshika",89,"CSE(Aiml)",20];
+console.log(array.includes("Vanshika"));
+console.log(array.includes(30));
