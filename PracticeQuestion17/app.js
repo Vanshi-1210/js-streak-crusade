9e33302dcf71d8477ba35ec223d10aeda87a55f7
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-09 18:20:11 +0530

    refactor: move quit logic inside while loop using break

--- PracticeQuestion17/app.js
+++ PracticeQuestion17/app.js
@@ -2,13 +2,15 @@ const favMovie="SecretSuperstar";
 
 let guess=prompt("guess my favourite movie");
 
-while((guess!=favMovie) && (guess!='quit')){
-
+while(guess!=favMovie){
+    if(guess=="quit"){
+        console.log("you quit")
+        break;
+    }
+    
      guess=prompt("Wrong guess.please Guess Again");
 }
 
 if(guess==favMovie){
     console.log("Congrats!!");
-}else{
-    console.log("You quit")
 }
