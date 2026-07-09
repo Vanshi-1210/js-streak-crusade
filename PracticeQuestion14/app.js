89ce157df8c7720349d785266ab11488e54c6fca
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-09 13:34:06 +0530

    feat: add solution to get the last n elements of an array

--- PracticeQuestion14/app.js
+++ PracticeQuestion14/app.js
@@ -1,15 +1,28 @@
-// Qs6. Write a JavaScript program to check if an element exists in an array or not.
 
-// let array=["Vanshika",89,"CSE(Aiml)",20];
-// console.log(array.includes("Vanshika"));
-// console.log(array.includes(30));
 
 // Qs1. Write a JavaScript program to get the first n elements of an array. [n can be any positive number].
 // For example: for array [7, 9, 0, -2] and n=3 -> Print, [7, 9, 0]
 
-let array = [7, 9, 0, -2 ];
+let Arr = [7, 9, 0, -2 ];
 let n = 3;
 
-let result = array.splice(0, n);
+let result = Arr.slice(0, n);
 
 console.log(result); 
+
+// Qs2. Write a JavaScript program to get the last n elements of an array. [n can be any positive number].
+// For example: for array [7, 9, 0, -2] and n=3 -> Print, [9, 0, -2]
+
+let Array=[ 7, 9 , 0 , -2];
+let m = 3;
+ 
+let output = Array.slice(-m);
+
+console.log(output);
+
+// Qs6. Write a JavaScript program to check if an element exists in an array or not.
+
+let array=["Vanshika",89,"CSE(Aiml)",20];
+console.log(array.includes("Vanshika"));
+console.log(array.includes(30));
+
