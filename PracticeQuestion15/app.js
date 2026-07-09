24d50c1dde119dd7090d91dd662eb3776ce87470
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-09 14:30:36 +0530

    feat: add solution to print odd numbers between 1 and 15

--- PracticeQuestion15/app.js
+++ PracticeQuestion15/app.js
@@ -4,3 +4,10 @@ for(let i=1;i<=5;i++){
     console.log(i);
 }
 
+// Qs.2. print odd numbers between 1 to 15 using loop.
+
+for(let i=1;i<=15;i++){
+    if(i%2!=0){
+        console.log(i);
+    }
+}
