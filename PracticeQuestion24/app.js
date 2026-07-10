35df9093672b899ac4a44edaedbb3e0a8897967f
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 20:42:50 +0530

    feat: add car object and print car name property

---
+++ PracticeQuestion24/app.js
@@ -0,0 +1,8 @@
+// Create an object representing a car that stores the following properties for the car:name,model,color.Print the car’s name.
+
+const car={
+    name:"Maruti",
+    model:800,
+    color:"blue"
+}
+console.log(car.name);
