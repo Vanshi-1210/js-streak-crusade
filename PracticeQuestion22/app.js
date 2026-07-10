1232d2c99ff225895c249566088bb102156c326b
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 17:34:09 +0530

    feat: implement nested objects example to store and access class information

---
+++ PracticeQuestion22/app.js
@@ -0,0 +1,24 @@
+const classInfo={
+    shradha:{
+        grade:"A+",
+        city:"Pune",
+    },
+
+    aman:{
+        grade:"A",
+        city:"Delhi",
+    },
+
+    karan:{
+        grade:"O",
+        city:"Mumbai",
+    },
+};
+
+console.log(classInfo);
+
+console.log(classInfo.shradha);
+
+console.log(classInfo.shradha.grade);
+
+console.log(classInfo.aman.city);
