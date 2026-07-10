ff707180b2a90be57e73d27e9e270a3b996e5259
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 17:44:52 +0530

    feat: implement array of objects example to store and access student data

--- PracticeQuestion22/app.js
+++ PracticeQuestion22/app.js
@@ -1,24 +1,44 @@
-const classInfo={
-    shradha:{
-        grade:"A+",
-        city:"Pune",
-    },
+// const classInfo={
+//     shradha:{
+//         grade:"A+",
+//         city:"Pune",
+//     },
 
-    aman:{
-        grade:"A",
-        city:"Delhi",
-    },
+//     aman:{
+//         grade:"A",
+//         city:"Delhi",
+//     },
 
-    karan:{
-        grade:"O",
-        city:"Mumbai",
-    },
-};
+//     karan:{
+//         grade:"O",
+//         city:"Mumbai",
+//     },
+// };
 
+// console.log(classInfo);
+// console.log(classInfo.shradha);
+// console.log(classInfo.shradha.grade);
+// console.log(classInfo.aman.city);
+
+
+// Array of objects.....
+const classInfo=[
+    {
+        name:"Vanshika",
+        age:20,
+        rollno:89
+    },
+    {
+        name:"hardik",
+        age:12,
+        rollno:90
+    },
+    {
+        name:"kratika",
+        age:17,
+        rollno:91
+    }
+];
 console.log(classInfo);
-
-console.log(classInfo.shradha);
-
-console.log(classInfo.shradha.grade);
-
-console.log(classInfo.aman.city);
+console.log(classInfo[1]);
+console.log(classInfo[0].name);
