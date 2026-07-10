b6566cf5f043a3f00056cb602c9ed64a14d5da03
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 18:30:15 +0530

    Add logic to generate random numbers using Math.random and Math.floor

---
+++ PracticeQuestion23/app.js
@@ -0,0 +1,9 @@
+//  generate a random number between 1 and 100..
+
+let step1=Math.random()*10;
+console.log(step1);
+let step2=(Math.floor(step1));
+console.log(step2);
+let step3=step2+1;
+console.log(step3);
+
