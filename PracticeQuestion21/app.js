f639345cd27493b08ae9d7f0635f63260004c789
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 15:13:39 +0530

    feat: create a social media post object literal example

---
+++ PracticeQuestion21/app.js
@@ -0,0 +1,11 @@
+// Practice Question: Create a JavaScript Object Literal to represent a social media post.
+// The object should store properties like username, content, likes, reposts, and tags.
+
+let post={
+    username:"@vanshikaagrawal",
+    content:"This is my #FirstPost",
+    likes:150,
+    repost:5,
+    tags:["@apnacollege","@sigma"]
+};
+console.log(post);
