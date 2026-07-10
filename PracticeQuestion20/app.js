c494374c3964c749ab9740b90b964b76dfb2ae90
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 13:20:23 +0530

    feat: implement sum of digits using string conversion approach

--- PracticeQuestion20/app.js
+++ PracticeQuestion20/app.js
@@ -1,8 +1,21 @@
 //Qs1. Print the factorial of a number n.
 
-let n=prompt("enter number");
-let fact=1;
-for(let i=1;i<=n;i++){
-    fact=fact*i;
+// let n=prompt("enter number");
+// let fact=1;
+// for(let i=1;i<=n;i++){
+//     fact=fact*i;
+// }
+// console.log(fact);
+
+//Qs2. finding the sum of digits in a number.
+
+let num = 287155;
+let numStr = num.toString(); 
+let sum = 0; 
+
+for (let i = 0; i < numStr.length; i++) {
+    sum = sum + parseInt(numStr[i]); 
 }
-console.log(fact);
+
+console.log(sum);
+
