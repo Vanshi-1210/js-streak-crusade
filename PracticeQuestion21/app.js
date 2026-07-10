30a5d811fa779929c082c309794c36139a4b1860
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 17:21:50 +0530

    feat: demonstrate add, update, and delete operations on object properties

--- PracticeQuestion21/app.js
+++ PracticeQuestion21/app.js
@@ -13,3 +13,22 @@ console.log(post.username);
 console.log(post.tags);
 console.log(post["content"]);
 console.log(post.tags[1]);
+
+//----------------------------------
+let student={
+    name:"vanshika",
+    age:20,
+    marks:94.4,
+    city:"Delhi",
+};
+student.city="Mumbai";
+console.log(student);
+//adding new key value pair
+student.gender="female";
+console.log(student);
+//updation
+student.marks=[89,90,96];
+console.log(student);
+//delete 
+delete student.gender;
+console.log(student);
