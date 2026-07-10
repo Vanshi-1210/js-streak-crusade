916a473b7228c739031b12f91c47d4f89d741282
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 14:12:50 +0530

    feat: add solution to delete all occurrences of an element from array

--- PracticeQuestion20/app.js
+++ PracticeQuestion20/app.js
@@ -1,33 +1,47 @@
 //Qs1. Print the factorial of a number n.
 
-let n=prompt("enter number");
-let fact=1;
-for(let i=1;i<=n;i++){
-    fact=fact*i;
-}
-console.log(fact);
+// let n=prompt("enter number");
+// let fact=1;
+// for(let i=1;i<=n;i++){
+//     fact=fact*i;
+// }
+// console.log(fact);
 
 //Qs2. finding the sum of digits in a number.
 
-let num = 287155;
-let numStr = num.toString(); 
-let sum = 0; 
+// let num = 287155;
+// let numStr = num.toString(); 
+// let sum = 0; 
 
-for (let i = 0; i < numStr.length; i++) {
-    sum = sum + parseInt(numStr[i]); 
-}
+// for (let i = 0; i < numStr.length; i++) {
+//     sum = sum + parseInt(numStr[i]); 
+// }
 
-console.log(sum);
+// console.log(sum);
 
 
 // Qs3. counting the no of digits in a number
 
 
-let num=28715223445;
-let numStr=num.toString();
-let count=0;
-for(let i=0;i<numStr.length;i++){
-    count=count+1;
+// let num=28715223445;
+// let numStr=num.toString();
+// let count=0;
+// for(let i=0;i<numStr.length;i++){
+//     count=count+1;
+// }
+// console.log(count);
+
+//Qs4. Write a JS program to delete all occurrences of element 'num' in a given array.
+// Example: if arr = [1, 2, 3, 4, 5, 6, 2, 3] & num = 2
+// Result should be arr = [1, 3, 4, 5, 6, 3]
+
+let arr=[1, 2, 3, 4, 5, 6, 2, 3];
+let num=2;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==num){
+        arr.splice(i,1);
+        i--;
+    }
 }
-console.log(count);
+console.log(arr);
 
