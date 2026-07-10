467aca7083f43809c93129b8f512e8cff40e6cd0
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 18:32:27 +0530

    feat: implement step-by-step random number generation between 1 and 5

--- PracticeQuestion23/app.js
+++ PracticeQuestion23/app.js
@@ -1,9 +1,17 @@
 //  generate a random number between 1 and 100..
 
-let step1=Math.random()*10;
+// let step1=Math.random()*10;
+// console.log(step1);
+// let step2=(Math.floor(step1));
+// console.log(step2);
+// let step3=step2+1;
+// console.log(step3);
+
+//  generate a random number between 1 and 5..
+
+let step1=Math.random()*5;
 console.log(step1);
 let step2=(Math.floor(step1));
 console.log(step2);
 let step3=step2+1;
 console.log(step3);
-
