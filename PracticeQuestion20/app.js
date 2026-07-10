08a415d1f22ae1965edec9731c34934b7b8b3c49
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-10 14:18:59 +0530

    feat: add solution for finding the largest number in an array

--- PracticeQuestion20/app.js
+++ PracticeQuestion20/app.js
@@ -1,35 +1,35 @@
 //Qs1. Print the factorial of a number n.
 
-// let n=prompt("enter number");
-// let fact=1;
-// for(let i=1;i<=n;i++){
-//     fact=fact*i;
-// }
-// console.log(fact);
+let n=prompt("enter number");
+let fact=1;
+for(let i=1;i<=n;i++){
+    fact=fact*i;
+}
+console.log(fact);
 
 //Qs2. finding the sum of digits in a number.
 
-// let num = 287155;
-// let numStr = num.toString(); 
-// let sum = 0; 
+let num = 287155;
+let numStr = num.toString(); 
+let sum = 0; 
 
-// for (let i = 0; i < numStr.length; i++) {
-//     sum = sum + parseInt(numStr[i]); 
-// }
+for (let i = 0; i < numStr.length; i++) {
+    sum = sum + parseInt(numStr[i]); 
+}
 
-// console.log(sum);
+console.log(sum);
 
 
 // Qs3. counting the no of digits in a number
 
 
-// let num=28715223445;
-// let numStr=num.toString();
-// let count=0;
-// for(let i=0;i<numStr.length;i++){
-//     count=count+1;
-// }
-// console.log(count);
+let num=28715223445;
+let numStr=num.toString();
+let count=0;
+for(let i=0;i<numStr.length;i++){
+    count=count+1;
+}
+console.log(count);
 
 //Qs4. Write a JS program to delete all occurrences of element 'num' in a given array.
 // Example: if arr = [1, 2, 3, 4, 5, 6, 2, 3] & num = 2
@@ -44,4 +44,15 @@ for(let i=0;i<arr.length;i++){
     }
 }
 console.log(arr);
+
+//Qs.5 finding the largest number from the array.
+
+let arr=[2,5,20,4,5,2,10];
+let largest=0;
+for(let i=0;i<arr.length;i++){
+    if(largest<arr[i]){
+        largest=arr[i];
+    }
+}
+console.log(largest);
 
