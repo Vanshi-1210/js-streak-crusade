58e86793b54be143ea0dc16505168ebe3f7bc289
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-11 13:31:54 +0530

    feat: add printPoem function

---
+++ PracticeQuestion25/app.js
@@ -0,0 +1,8 @@
+// Create a function that prints a poem to the console and call it.
+
+function printPoem(){
+    console.log("Twinkle Twinkle, little star");
+    console.log("how I wonder what you are");
+}
+
+printPoem();
