aad8bdcdc5a5c3ad3f3aba08362faca1bc28435a
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-11 13:15:10 +0530

    feat: add random dice roll generator

--- PracticeQuestion24/app.js
+++ PracticeQuestion24/app.js
@@ -17,3 +17,7 @@ const person={
 person.city="New York";
 person.country="United States";
 console.log(person);
+
+// Create a program that generates a random number representing a dice roll.[The number should be between 1 and 6].
+let diceRoll=Math.floor(Math.random()*6)+1;
+console.log(diceRoll);
