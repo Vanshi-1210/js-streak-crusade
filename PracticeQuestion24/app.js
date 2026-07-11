ba2a9d7a787ec768251feda1a961255b33beb76e
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-11 13:09:38 +0530

    feat: add person object, update city, and add country property

--- PracticeQuestion24/app.js
+++ PracticeQuestion24/app.js
@@ -1,8 +1,19 @@
 // Create an object representing a car that stores the following properties for the car:name,model,color.Print the car’s name.
 
-const car={
-    name:"Maruti",
-    model:800,
-    color:"blue"
+// const car={
+//     name:"Maruti",
+//     model:800,
+//     color:"blue"
+// }
+// console.log(car.name);
+
+//Create an object Person with their name,age and city.Edit their city’s original value to change it to “New York”.Add a new property country and set it to the United States.
+
+const person={
+    name:"Vanshika",
+    age:20,
+    city:"Aligarh",
 }
-console.log(car.name);
+person.city="New York";
+person.country="United States";
+console.log(person);
