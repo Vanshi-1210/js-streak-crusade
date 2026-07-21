1f507a0f12b76f0cd3dafc54c58f0170b4e80de9
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-07-21 22:28:52 +0530

    feat: limit setInterval execution to print message 5 times

--- PracticeQuestion27/app.js
+++ PracticeQuestion27/app.js
@@ -4,3 +4,16 @@ const square= n =>{
     return n*n;
 }
 console.log(square(8));
+
+// write a function that prints "Hello World" 5 times at intervals of 2s Each ..
+
+let count = 0;
+
+let id = setInterval(() => {
+    console.log("Hello World");
+    count++;
+    
+    if (count === 5) {
+        clearInterval(id);
+    }
+}, 2000);
