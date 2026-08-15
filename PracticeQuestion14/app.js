439093214a6d3d9c2ad4abc704c7a6b4d3ab2e7e
Vanshi-1210 <vanshikaagrawal765@gmail.com>
2026-08-15 14:39:05 +0530

    feat: add check for lowercase character at specific index

--- PracticeQuestion14/app.js
+++ PracticeQuestion14/app.js
@@ -3,46 +3,56 @@
 // Qs1. Write a JavaScript program to get the first n elements of an array. [n can be any positive number].
 // For example: for array [7, 9, 0, -2] and n=3 -> Print, [7, 9, 0]
 
-let Arr = [7, 9, 0, -2 ];
-let n = 3;
+// let Arr = [7, 9, 0, -2 ];
+// let n = 3;
 
-let result = Arr.slice(0, n);
+// let result = Arr.slice(0, n);
 
-console.log(result); 
+// console.log(result); 
 
 // Qs2. Write a JavaScript program to get the last n elements of an array. [n can be any positive number].
 // For example: for array [7, 9, 0, -2] and n=3 -> Print, [9, 0, -2]
 
-let Array=[ 7, 9 , 0 , -2];
-let m = 3;
+// let Array=[ 7, 9 , 0 , -2];
+// let m = 3;
  
-let output = Array.slice(-m);
+// let output = Array.slice(-m);
 
-console.log(output);
+// console.log(output);
 
 // Qs3. Write a JavaScript program to check whether a string is blank or not.
 
-let str = "   "; 
+// let str = "   "; 
 
-if (str.trim().length === 0) {
-    console.log("The string is blank.");
-} else {
-    console.log("The string is not blank.");
+// if (str.trim().length === 0) {
+//     console.log("The string is blank.");
+// } else {
+//     console.log("The string is not blank.");
+// }
+
+// Qs4.Write a JavaScript program to test whether the character at the given(character) index is lowercase
+
+let name="VAnsHiKa";
+let idx=5;
+if(name[idx] == name[idx].toLowerCase()){
+    console.log("Character is lowercase");
+}else{
+    console.log("Character is not lowercase");
 }
 
 // Qs5. Write a JavaScript program to strip leading and trailing spaces from a string.
 
-let str = "   Hello World!   ";
+// let str = "   Hello World!   ";
 
-let strippedStr = str.trim();
+// let strippedStr = str.trim();
 
-console.log(strippedStr);
+// console.log(strippedStr);
 
 // Qs6. Write a JavaScript program to check if an element exists in an array or not.
 
-let array=["Vanshika",89,"CSE(Aiml)",20];
-console.log(array.includes("Vanshika"));
-console.log(array.includes(30));
+// let array=["Vanshika",89,"CSE(Aiml)",20];
+// console.log(array.includes("Vanshika"));
+// console.log(array.includes(30));
 
 
 
